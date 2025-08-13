878169f2e8e822ee8e73b4312e582f6c2d817907
hemanth5718 <hemanthgowda571@gmail.com>
2025-08-13 10:03:51 +0530

    h

--- adv_sel/src/test/java/scenario/Google.java
+++ adv_sel/src/test/java/scenario/Google.java
@@ -3,6 +3,7 @@ package scenario;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,7 +12,7 @@ import org.testng.annotations.Test;
 
 public class Google {
 	@Test
-	public void google() {
+	public void google() throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
 		driver.manage().window().maximize();
@@ -22,8 +23,17 @@ public class Google {
 		WebElement ele = driver.findElement(By.xpath("//a[@data-pid='421']"));
 		Actions act = new Actions(driver);
 		act.scrollToElement(ele).perform();
-		ele.click();		
-		WebElement ele1 = driver.findElement(By.xpath("(//h2[contains(@class,'R3mvd') and contains(text(),'Welcome')])[2]"));
+		ele.click();	
+		//
+		Thread.sleep(1000);
+		System.out.println("D:"+driver.getTitle());
+		act.sendKeys(Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.ENTER).perform();
+		//act.click().perform();
+		WebElement ele1 = driver.findElement(By.xpath("//a[@ve-visible=\"false\"]/preceding-sibling::h2[text()='Welcome to the Chrome Web Store']"));
+	//	driver.switchTo().frame(0);
+//	    SearchContext shadow = driver.findElement(By.xpath("//div[@class='wcXMWc']"));
+//	    String ele2 = shadow.findElement(By.cssSelector("//h2[@class='R3mvd']")).getText();
+//	    
 		String ele2 = ele1.getText();
 		System.out.println(ele2);
 		
